2951a2b7f1853e8157b1804150723bcebc11fbd8
Niyati Chandrasekar <niyati.chandrasekar@gmail.com>
2026-03-29 22:19:39 +0000

    Homework Submitted

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,24 +1,32 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
+luckyNumber = 5;  
+console.log(luckyNumber);
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
-
+sleepHours = 10;
+console.log(sleepHours);
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
+favoriteFruit = "blueberry";
+console.log("My favorite fruit is " + favoriteFruit);
 
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
-
-
+city = "Portland"
+country = "America"
+console.log(city);
+console.log(country);
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+name = "Max";
+age = 4;
+type = "Maltipoo";
+console.log(" My dog's name is " + name + " and he is " + age + " and is a " + type); 
